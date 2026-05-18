c0f09188e9f5f17e2b576f4b41e6158c29dad5ec
airjaldirbb <admin@airjaldi.net>
2026-05-18 10:03:11 +0530

    latest

--- pages/api/barcodeEnabled.js
+++ pages/api/barcodeEnabled.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
     if (req.method !== "GET") {
       return res.status(405).json({ message: "Method not allowed" });
-    }
+    }Start
 
     const items = await Item.find({ barcodeTracking: "ENABLE" });
 
